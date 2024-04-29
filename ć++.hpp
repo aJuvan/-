2fc67883cadaa3cb9936560830399f6572db0c70
Andraž Juvan <andraz.juvan@shekaj.si>
2024-04-29 19:31:19 +0200

    Added header ć++.hpp

---
+++ ć++.hpp
@@ -0,0 +1,68 @@
+#ifndef ĆPP_HPP
+#define ĆPP_HPP
+
+#define istina bool
+#define cijeli_broj int
+#define dugi_broj long
+#define kratak_broj short
+#define bajt byte
+#define znak char
+#define zajla string
+#define decimalni_broj float
+#define dugi_decimalni_broj double
+#define pokazivač *
+#define nepotpisan unsigned
+#define praznina void
+
+#define nešto auto
+#define nema NULL
+#define je true
+#define nije false
+
+#define glavni main
+#define vrati return
+
+#define struktura struct
+#define klasa class
+#define ovaj this
+#define stvori new
+#define pucaj delete
+
+#define stalan const
+#define nabroj enum
+#define šaban friend
+#define marš goto
+#define pokušaj try
+#define uhvati catch
+#define baci throw
+#define izuzetak exception
+#define virtualan virtual
+
+#define možda if
+#define nemože else
+#define svaki for
+#define dok while
+#define radi do
+#define biraj switch
+#define slučaj case
+#define jebeš break
+#define jebi_ga continue
+#define prirodno default
+
+#define ime_prostora namespace
+#define trebaj using
+#define majkemi assert
+#define šablona template
+#define veličina sizeof
+
+#define moja_majka private
+#define naša_majka protected
+#define tvoja_majka public
+#define statički static
+
+#define cizlaz cout
+#define cgreška cerr
+#define culaz cin
+#define kraj endl
+
+#endif
